4a23426fcbe34aec3806772a746d0aa41c835cb2
sauravrvce <sauravrvce@gmail.com>
2023-10-08 04:14:25 +0530

    made corrections to TCs to work with new get_hparam_combinations

--- test_utils.py
+++ test_utils.py
@@ -7,8 +7,12 @@ def test_hparam_combinations():
     # test case to check whether all possible hparam combos are generated
     gamma_list = [0.001, 0.01, 0.1, 1]
     C_list = [1, 10, 100, 1000]
+    svm_hparams = {}
+    svm_hparams['gamma'] = gamma_list
+    svm_hparams['C'] = C_list
 
-    h_params_combinations = get_hparam_combinations(gamma_list, C_list)
+    # Create a list of dictionaries for all hparam combinations
+    h_params_combinations = get_hparam_combinations(svm_hparams)
 
     assert len(h_params_combinations) == len(gamma_list) * len(C_list)
     
@@ -17,8 +21,12 @@ def test_specific_combinations():
 # test case to check whether all possible hparam combos are generated
     gamma_list = [0.001, 0.01, 0.1, 1]
     C_list = [1, 10, 100, 1000]
+    svm_hparams = {}
+    svm_hparams['gamma'] = gamma_list
+    svm_hparams['C'] = C_list
 
-    h_params_combinations = get_hparam_combinations(gamma_list, C_list)
+    # Create a list of dictionaries for all hparam combinations
+    h_params_combinations = get_hparam_combinations(svm_hparams)
 
     assert len(h_params_combinations) == len(gamma_list) * len(C_list)
 
@@ -48,21 +56,25 @@ def test_modelSave_modelType():
     
     gamma_list = [0.001, 0.01]
     C_list = [1, 2]
+    svm_hparams = {}
+    svm_hparams['gamma'] = gamma_list
+    svm_hparams['C'] = C_list
 
-    h_params_combinations = get_hparam_combinations(gamma_list, C_list)
+    # Create a list of dictionaries for all hparam combinations
+    h_params_combinations = get_hparam_combinations(svm_hparams)
 
     test_size = 0.1
     dev_size = 0.6
     train_size = 1-(test_size+dev_size)
     x_train, x_dev, x_test, y_train, y_dev, y_test = split_train_dev_test(X, y, test_size, dev_size)
 
-    clf = SVC
+    clf = 'svm'
 
     outputs = tune_hparams(x_train, y_train, x_dev, y_dev, h_params_combinations, clf, train_size, dev_size, test_size)
     actual_model_path = outputs[-1]
 
     assert(os.path.exists(actual_model_path)==True)
-    assert type(load(actual_model_path)) == clf
+    assert type(load(actual_model_path)) == SVC
 
 
 
