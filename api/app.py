c13b09267767741e349e407d5630cdd3c48ac641
sauravrvce <sauravrvce@gmail.com>
2023-10-29 16:53:24 +0530

    code to take curl request and predict

--- api/app.py
+++ api/app.py
@@ -1,9 +1,18 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
+from flask_session import Session
 import numpy as np
 from joblib import load
+import os
 
+model_name = 'svm_train_0.6_dev_0.2_test_0.2_gamma_0.001_C_1.joblib'
+predicted_result = 'None'
 
 app = Flask(__name__)
+
+app.secret_key = '#$@urav'  # Set a secret key for session management
+app.config['SESSION_TYPE'] = 'filesystem'  # Choose the session storage type (filesystem in this case)
+app.config['SESSION_PERMANENT'] = False  # Sessions are not permanent by default
+Session(app)
 
 @app.route("/hello/<val>")
 def hello_world(val):
@@ -14,10 +23,22 @@ def sum_num(x,y):
     sum = int(x) + int(y)
     return str(sum)
 
-@app.route("/model", methods = ['POST'])
-def pred_model():
+@app.route("/predict", methods = ['POST'])
+def predict():
     
-    js = request.get_json( )
-    x = js['x']
-    y = js['y']
-    return x+y
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    models_dir = os.path.join(current_dir, '..', 'models')
+    model_file_path = os.path.join(models_dir, model_name)
+
+    # Load model
+    model = load(model_file_path)
+
+    # Get the input data as a JSON object
+    data = request.get_json(force=True)
+    input_vector = np.array(data['image']).reshape(1, -1)
+    
+    # Make predictions using the loaded model
+    prediction = model.predict(input_vector)
+    result = prediction[0]
+    return "Predicted Digit = " + str(result)
+
