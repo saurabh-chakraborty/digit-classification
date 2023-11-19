8272e5caed511c80cf7b0d8513240411212b7ba4
sauravrvce <sauravrvce@gmail.com>
2023-11-19 16:35:52 +0530

    temp comment to avoide delay in git actions run

--- docker_run.sh
+++ docker_run.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Docker build command
-docker build -t digiclf:v3 -f docker/Dockerfile .
+# # Docker build command
+# docker build -t digiclf:v3 -f docker/Dockerfile .
 
-# Docker run command with volume mounting (put models in volume) 
-docker run -v /home/sauravrvce/mlops/digit-classification/models:/models digiclf:v3
+# # Docker run command with volume mounting (put models in volume) 
+# docker run -v /home/sauravrvce/mlops/digit-classification/models:/models digiclf:v3
